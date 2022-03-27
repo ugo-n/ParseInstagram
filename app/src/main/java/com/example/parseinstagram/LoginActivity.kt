131ee3f91956809eccc6b72e64b9826b20b86c3e
ugo-n <eagle.nnakwe@gmail.com>
2022-03-27 11:34:14 -0400

    Initial commit

---
+++ app/src/main/java/com/example/parseinstagram/LoginActivity.kt
@@ -0,0 +1,23 @@
+package com.example.parseinstagram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        findViewById<Button>(R.id.bt_login).setOnClickListener{
+            val username = findViewById<EditText>(R.id.et_username).text.toString()
+            val password = findViewById<EditText>(R.id.et_password).text.toString()
+            loginUser(username, password)
+        }
+    }
+
+    private fun loginUser(username: String, password: String){
+
+    }
+}
---
+++ app/src/main/java/com/example/parseinstagram/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.parseinstagram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * Let user create a post by taking a photo with their camera
+ */
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
