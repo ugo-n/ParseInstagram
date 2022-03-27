9f12e8455ff16c42f6757c3dc2cf0c5c751d5f3a
ugo-n <eagle.nnakwe@gmail.com>
2022-03-27 13:11:44 -0400

    Implement Post ParseObject Class

--- app/src/main/java/com/example/parseinstagram/MainActivity.kt
+++ app/src/main/java/com/example/parseinstagram/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.parseinstagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.parse.FindCallback
+import com.parse.ParseException
+import com.parse.ParseQuery
 
 /**
  * Let user create a post by taking a photo with their camera
@@ -12,6 +16,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        queryPosts()
 
+    }
+
+    //Query for all posts in out server
+    fun queryPosts(){
+        // Specify which class to query
+        val query: ParseQuery<Post> = ParseQuery.getQuery(Post::class.java)
+        // Find all Post objects
+        query.include(Post.KEY_USER)
+        query.findInBackground(object: FindCallback<Post> {
+            override fun done(posts: MutableList<Post>?, e: ParseException?) {
+                if(e != null){
+                    // Something went wrong
+                    Log.e(TAG, "Error fetching posts")
+                } else {
+                    if (posts != null){
+                        for(post in posts){
+                            Log.i(TAG, "Post: " + post.getDescription() + ", username: " + post.getUser()?.username)
+                        }
+                    }
+                }
+            }
+
+        })
+    }
+
+    companion object{
+        const val TAG = "MainActivity"
     }
 }
