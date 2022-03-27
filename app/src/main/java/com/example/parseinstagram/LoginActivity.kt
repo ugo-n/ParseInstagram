90e8ff5723fe97fd9b9a223307cb3a1a0ba8e6ff
ugo-n <eagle.nnakwe@gmail.com>
2022-03-27 12:51:11 -0400

    Implement Login and Sign Up

--- app/src/main/java/com/example/parseinstagram/LoginActivity.kt
+++ app/src/main/java/com/example/parseinstagram/LoginActivity.kt
@@ -1,23 +1,82 @@
 package com.example.parseinstagram
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.parse.ParseObject
+import com.parse.ParseUser
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        // Check if there's a user logged in
+        // If there is, take them to MainActivity
+
+        if(ParseUser.getCurrentUser() != null){
+            goToMainActivity()
+        }
+
         findViewById<Button>(R.id.bt_login).setOnClickListener{
             val username = findViewById<EditText>(R.id.et_username).text.toString()
             val password = findViewById<EditText>(R.id.et_password).text.toString()
             loginUser(username, password)
         }
+
+        findViewById<Button>(R.id.bt_signup).setOnClickListener{
+            val username = findViewById<EditText>(R.id.et_username).text.toString()
+            val password = findViewById<EditText>(R.id.et_password).text.toString()
+            signUpUser(username, password)
+        }
+    }
+
+    private fun signUpUser(username: String, password: String){
+        // Create the ParseUser
+        val user = ParseUser()
+
+        // Set fields for the user to be created
+        user.setUsername(username)
+        user.setPassword(password)
+
+        user.signUpInBackground { e ->
+            if (e == null) {
+                // User has successfully created a new account
+
+                Toast.makeText(this, "Created account successfully", Toast.LENGTH_SHORT).show()
+                goToMainActivity()
+
+            } else {
+                e.printStackTrace()
+                Toast.makeText(this, "Error creating account", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     private fun loginUser(username: String, password: String){
+        ParseUser.logInInBackground(username, password, ({ user, e ->
+            if (user != null) {
+                Log.i(TAG, "Successfully logged in user")
+                goToMainActivity()
+            } else {
+                e.printStackTrace()
+                Toast.makeText(this, "Error logging in", Toast.LENGTH_SHORT).show()
+            }})
+        )
 
+    }
+
+    private fun goToMainActivity(){
+        val intent = Intent(this@LoginActivity, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    companion object{
+        const val TAG = "LoginActivity"
     }
 }
