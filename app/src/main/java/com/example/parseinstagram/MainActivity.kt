45ee44fa6e192c2c876dc5a885ffb86ef7bd6df7
ugo-n <eagle.nnakwe@gmail.com>
2022-04-04 01:25:33 -0400

    Create Feed and Profile Fragment

--- app/src/main/java/com/example/parseinstagram/MainActivity.kt
+++ app/src/main/java/com/example/parseinstagram/MainActivity.kt
@@ -6,6 +6,7 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
+import android.provider.ContactsContract
 import android.provider.MediaStore
 import android.util.Log
 import android.widget.Button
@@ -17,6 +18,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.example.parseinstagram.fragments.ComposeFragment
 import com.example.parseinstagram.fragments.FeedFragment
+import com.example.parseinstagram.fragments.ProfileFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.parse.*
 import java.io.File
@@ -27,17 +29,11 @@ import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
-
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val fragmentManager: FragmentManager = supportFragmentManager
-
-
-
 
         findViewById<BottomNavigationView>(R.id.bottom_navigation).setOnItemSelectedListener {
             item ->
@@ -47,14 +43,12 @@ class MainActivity : AppCompatActivity() {
             when(item.itemId){
                 R.id.action_home -> {
                     fragmentToShow = FeedFragment()
-                    //Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show()
                 }
                 R.id.action_compose -> {
                     fragmentToShow = ComposeFragment()
-                    //Toast.makeText(this, "Compose", Toast.LENGTH_SHORT).show()
                 }
                 R.id.action_profile -> {
-                    Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show()
+                    fragmentToShow = ProfileFragment()
                 }
 
             }
@@ -79,28 +73,7 @@ class MainActivity : AppCompatActivity() {
 
 
 
-    //Query for all posts in out server
-    fun queryPosts(){
-        // Specify which class to query
-        val query: ParseQuery<Post> = ParseQuery.getQuery(Post::class.java)
-        // Find all Post objects
-        query.include(Post.KEY_USER)
-        query.findInBackground(object: FindCallback<Post> {
-            override fun done(posts: MutableList<Post>?, e: ParseException?) {
-                if(e != null){
-                    // Something went wrong
-                    Log.e(TAG, "Error fetching posts")
-                } else {
-                    if (posts != null){
-                        for(post in posts){
-                            Log.i(TAG, "Post: " + post.getDescription() + ", username: " + post.getUser()?.username)
-                        }
-                    }
-                }
-            }
 
-        })
-    }
 
     companion object{
         const val TAG = "MainActivity"
--- app/src/main/java/com/example/parseinstagram/fragments/FeedFragment.kt
+++ app/src/main/java/com/example/parseinstagram/fragments/FeedFragment.kt
@@ -1,14 +1,31 @@
 package com.example.parseinstagram.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.parseinstagram.MainActivity
+import com.example.parseinstagram.Post
+import com.example.parseinstagram.PostAdapter
 import com.example.parseinstagram.R
+import com.parse.FindCallback
+import com.parse.ParseException
+import com.parse.ParseQuery
 
 
-class FeedFragment : Fragment() {
+open class FeedFragment : Fragment() {
+
+    lateinit var postRecyclerView: RecyclerView
+
+    lateinit var adapter: PostAdapter
+
+    var allPosts: MutableList<Post> = mutableListOf()
+
+    var postLimit = 20
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,6 +33,53 @@ class FeedFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_feed, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //this is where we set up our views
+
+        postRecyclerView = view.findViewById(R.id.postRecyclerView)
+
+        adapter = PostAdapter(requireContext(), allPosts)
+
+        postRecyclerView.adapter = adapter
+
+        postRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        queryPosts()
+
+    }
+
+    //Query for all posts in out server
+    open fun queryPosts(){
+        // Specify which class to query
+        val query: ParseQuery<Post> = ParseQuery.getQuery(Post::class.java)
+        // Find all Post objects
+        query.include(Post.KEY_USER)
+        query.addDescendingOrder("createdAt").limit = postLimit
+        query.findInBackground(object: FindCallback<Post> {
+            override fun done(posts: MutableList<Post>?, e: ParseException?) {
+                if(e != null){
+                    // Something went wrong
+                    Log.e(TAG, "Error fetching posts")
+                } else {
+                    if (posts != null){
+                        for(post in posts){
+                            Log.i(TAG, "Post: " + post.getDescription() + ", username: " + post.getUser()?.username)
+                        }
+
+                        allPosts.addAll(posts)
+                        adapter.notifyDataSetChanged()
+                    }
+                }
+            }
+
+        })
+    }
+
+    companion object{
+        const val TAG = "FeedFragment"
     }
 
 }
